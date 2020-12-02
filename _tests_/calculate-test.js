65f7e2e76f789115af08e47adb153313ecf8edf3
tGodson <tendongze95@gmail.com>
2020-12-02 10:26:50 +0100

    add test to project

---
+++ _tests_/calculate-test.js
@@ -0,0 +1,9 @@
+describe("calculate", () => {
+    it("passing test", () => {
+      expect(true).toBeTruthy();
+    });
+  
+    it("failing test", () => {
+      expect(false).toBeFalsy();
+    });
+  });
